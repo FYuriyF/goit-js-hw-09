ea6e6c427123a3942c9b9e828121fba4ed1ebe57
FYuriyF <jufedor@gmail.com>
2023-03-23 17:07:24 +0200

    firts commit

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  form: document.querySelector('form'),
+  inputDelay: document.querySelector('input[name="delay"]'),
+  inputStep: document.querySelector('input[name="step"]'),
+  inputAmount: document.querySelector('input[name="amount"]'),
+};
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+// Number(refs.inputDelay.value);
+// Number(refs.inputAmount.value);
+// Number(refs.inputStep.value);
+
+function sumbitPromise(evt) {
+  evt.prevenDefault();
+  let prevDelay;
+  for (let i = 0; i <= refs.inputAmount.value; i += 1) {
+    prevDelay += Number(refs.inputStep.value);
+    createPromise(i, prevDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
   }
 }
